fe79dffcffa7013bdd015a026946019cd6404a9a
parsa sadeghi tafti <p.sadeghitafti@email.kntu.ac.ir>
2024-06-25 00:08:47 +0330

    init backend commit

---
+++ backend/db_project_backend/src/main/java/ir/ac/kntu/db_project_backend/DbProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package ir.ac.kntu.db_project_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbProjectBackendApplication.class, args);
+	}
+
+}
