7946dfb7dcccacd76ead82cfd167b0e2004d4acb
parsa sadeghi tafti <p.sadeghitafti@email.kntu.ac.ir>
2024-07-05 13:07:29 +0330

    add directory for sql, added not null statements

--- SQL/MakeTables.sql
+++ SQL/MakeTables.sql
@@ -1,48 +1,50 @@
--- Active: 1716117406866@@localhost@3306@main
+-- Active: 1716876444801@@localhost@3306@main
 CREATE TABLE Accounts (
-    AID INT NOT NULL,
-    FirstName VARCHAR(50),
-    LastName VARCHAR(50),
-    Phone VARCHAR(50),
-    Email VARCHAR(50),
-    ActiveCity VARCHAR(50),
-    PRIMARY KEY (AID)
+    AID INT UNIQUE AUTO_INCREMENT,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Phone VARCHAR(50) NOT NULL,
+    Email VARCHAR(50) NOT NULL,
+    ActiveCity INT NOT NULL,
+    PRIMARY KEY (AID),
+    Foreign Key (ActiveCity) REFERENCES Cities(id)
 );
 
 CREATE TABLE Businesses (
-    BID INT NOT NULL,
+    BID A INT NOT NULL,
     OID INT NOT NULL,
-    Type VARCHAR(50),
-    SerialNUMBER INT,
-    Name VARCHAR(50),
+    Type VARCHAR(50) NOT NULL,
+    SerialNUMBER INT NOT NULL,
+    Name VARCHAR(50) NOT NULL,
     PRIMARY KEY (BID),
     FOREIGN KEY (AID) REFERENCES Accounts(AID)
 );
 
 CREATE TABLE Addresses(
     BID INT NOT NULL, 
-    Address VARCHAR(255),
+    Address VARCHAR(255) NOT NULL,
     CID INT NOT NULL,
     FOREIGN KEY (BID) REFERENCES Businesses(BID),
     FOREIGN KEY (CID) REFERENCES Cities(id)
 );
 
 CREATE TABLE Advertisements (
-    ADDID INT PRIMARY KEY,
-    BID INT,
-    AID INT,
-    Description TEXT,
-    Title VARCHAR(50),
-    Location VARCHAR(255),
-    Date DATE,
-    Price INT,
+    ADDID INT PRIMARY KEY NOT NULL,
+    BID INT NOT NULL,
+    AID INT NOT NULL,
+    Description TEXT NOT NULL,
+    Title VARCHAR(50) NOT NULL,
+    Location VARCHAR(255) NOT NULL,
+    DateModified DATE NOT NULL,
+    Price INT NOT NULL,
+    IsNew BIT NOT NULL,
     FOREIGN KEY (BID) REFERENCES Businesses(BID),
     FOREIGN KEY (AID) REFERENCES Accounts(AID)
 );
 
 CREATE TABLE Accesses (
-    BID INT,
-    AID INT,
+    BID INT NOT NULL,
+    AID INT NOT NULL,
     Level TINYINT,
     FOREIGN KEY (BID) REFERENCES Businesses(BID),
     FOREIGN KEY (AID) REFERENCES Accounts(AID)
@@ -50,17 +52,29 @@ CREATE TABLE Accesses (
 
 CREATE TABLE Comments (
     ADDID INT ,
-    Type VARCHAR(50),
+    Type TINYINTINT,
     Description TEXT,
     FOREIGN KEY (ADDID) REFERENCES Advertisements(ADDID)
+    Foreign Key (Type) REFERENCES Type(ID)
 );
 
 CREATE TABLE AddStatus (
+    Status BIT,
     ADDID INT PRIMARY KEY,
     AdminNote TEXT,
     LastUpdated DATE,
     FOREIGN KEY (ADDID) REFERENCES Advertisements(ADDID)
+    Foreign Key (Status) REFERENCES Status(ID)
 );
+
+create TABLE Status(
+    ID BIT,
+    Status VARCHAR(8)
+);
+
+INSERT INTO Status(ID, Status) VALUES 
+    (1, 'Accepted'),
+    (0, 'Declined');
 
 CREATE TABLE Images (
     ADDID INT,
@@ -68,4 +82,29 @@ CREATE TABLE Images (
     FOREIGN KEY (ADDID) REFERENCES Advertisements(ADDID)
 );
 
+CREATE TABLE Admins(
+    AID INT NOT NULL,
+    Foreign Key (AID) REFERENCES Accounts(AID)
+);
+
+CREATE TABLE Type(
+    ID TINYINT NOT NULL,
+    Name VARCHAR(30),
+    PRIMARY KEY (ID)
+);
+
+INSERT INTO Type(ID, Name) VALUES 
+    (1,'positive feedback'),
+    (2, 'negative feedback(complaint)'),
+    (3, 'brand value'),
+    (4, 'other');
+
+CREATE TABLE Access(
+    Code BIT PRIMARY KEY UNIQUE NOT NULL,
+    AccessLevel VARCHAR(10) NOT NULL
+);
+
+INSERT INTO access(Code, AccessLevel) VALUES
+    (0, 'Owner'),
+    (1, 'Manager');
 
