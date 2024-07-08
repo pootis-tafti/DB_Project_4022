6ef99b3a934a78e6f80c27a4c21aef3b2a3f7296
parsa sadeghi tafti <p.sadeghitafti@email.kntu.ac.ir>
2024-07-08 09:12:21 +0330

    added status change annotation

--- backend/db_project_backend/src/main/java/ir/ac/kntu/db_project_backend/mappers/AccountRowMapper.java
+++ backend/db_project_backend/src/main/java/ir/ac/kntu/db_project_backend/mappers/AccountRowMapper.java
@@ -4,21 +4,22 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.lang.NonNull;
 
+import io.micrometer.common.lang.Nullable;
 import ir.ac.kntu.db_project_backend.models.Account;
 
 public class AccountRowMapper implements RowMapper<Account> {
 
     @Override
-    @NonNull
-    public Account mapRow(@NonNull ResultSet rs, int rowNum) throws SQLException {
+    @Nullable
+    public Account mapRow(ResultSet rs, int rowNum) throws SQLException {
         Account account = new Account();
         account.setId(rs.getInt("AID"));
         account.setEmail(rs.getNString("Email"));
         account.setPhoneNumber(rs.getString("Phone"));
         account.setActiveCity(rs.getString("ActiveCity"));
         account.setActiveProvince(rs.getString("ActiveProvince"));
+        account.setStatus(rs.getBoolean("Status"));
         return account;
     }
 }
